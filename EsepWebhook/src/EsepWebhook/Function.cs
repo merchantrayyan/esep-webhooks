1d2b8aad84bd68e843faf27ce4e3c2dd6a454c8b
merchantrayyan <rayyan.merchant@gmail.com>
2023-11-12 18:27:14 -0500

    done

--- EsepWebhook/src/EsepWebhook/Function.cs
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -9,14 +9,7 @@ namespace EsepWebhook;
 
 public class Function
 {
-    
-    /// <summary>
-    /// A simple function that takes a string and does a ToUpper
-    /// </summary>
-    /// <param name="input"></param>
-    /// <param name="context"></param>
-    /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public string FunctionHandler(object input, ILambdaContext context)
     {
         dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
         
@@ -34,3 +27,5 @@ public class Function
         return reader.ReadToEnd();
     }
 }
+
+
